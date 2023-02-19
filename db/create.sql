7bcfa2ee62342e1d0cb288e9ac5c77ad53d29a21
LukasH010 <93194660+LukasH010@users.noreply.github.com>
2023-02-19 21:11:45 +0000

    kleine aanpassing

--- db/create.sql
+++ db/create.sql
@@ -2,6 +2,7 @@
 -- create tables
 --
 
+--Tabel voor producten
 CREATE TABLE products (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   code VARCHAR(15),
@@ -12,6 +13,7 @@ CREATE TABLE products (
   accessories_id VARCHAR
 );
 
+--Tabel voor reviews 
 CREATE TABLE reviews (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
  auteur_id INTEGER,
@@ -20,17 +22,20 @@ id INTEGER PRIMARY KEY AUTOINCREMENT,
   rating NUMERIC(1, 10)
   );
 
+--Tabel voor auteur (van review)
 CREATE TABLE auteur (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
  name VARCHAR(255)
 );
 
+--Tabel voor producten wat kleding is
 CREATE TABLE Clothes (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   products_id INTEGER,
    name TEXT
 );
 
+--Tabel voor producten wat voorwerpen zijn
 CREATE TABLE accessories(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   products_id INTEGER,
@@ -48,7 +53,7 @@ CREATE TABLE accessories(
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
---informatie voor table producten
+--informatie voor tabel producten
 insert into products (name, description, code, price, clothes_id ) values ('Shoes', 'Guerilla Gym exclusive for Men and Women. ', '000674978-0', 49.99, 1);
 insert into products (name, description, code, price, clothes_id) values ('T-Shirts', 'Guerilla Gym exclusive for Men and Women.', '077030122-3', 24.99, 2);
 insert into products (name, description, code, price, accessories_id) values ('Straps', 'Guerilla Gym exclusive for the best experience.', '555924201-X', 13.99, 1);
@@ -62,7 +67,7 @@ insert into products (name, description, code, price, clothes_id) values ('Glove
 insert into products (name, description, code, price, clothes_id) values ('Cap', 'Guerilla Gym exclusive . ', '992666023-7', 12.99, 7);
 insert into products (name, description, code, price,  accessories_id) values ('Shakecup', 'Guerilla Gym exclusive made for the shake. ', '912650523-7', 4.99, 5);
 
--- informatie voor table reviews
+-- informatie voor tabel reviews
 insert into reviews(auteur_id, opmerking, rating,  product_id) values ( 3, 'Super kwaliteit ondanks dat het goedkoop is!', '8/10', 1);
 insert into reviews(auteur_id, opmerking, rating, product_id) values (2, 'Het tshirt is van goede kwaliteit en zit erg comfortabel.', '8/10', 2);  
 insert into reviews(auteur_id, opmerking, rating, product_id) values (1, 'Mijn bestelling kwam erg snel en zonder problemen binnen.', '9/10', 3);
@@ -78,7 +83,7 @@ insert into auteur (name) values ('gymmeneer123');
 insert into auteur (name) values ('Mnr. de Voet');
 insert into auteur (name) values ('Mvr. ei');
 
--- clothes
+-- kleding
 insert into  clothes (name, products_id) values ('Shoes', 1);
 insert into  clothes (name, products_id) values ('T-shirts', 2);
 insert into  clothes (name, products_id) values ('Hoodies & Sweaters', 5);
@@ -87,7 +92,7 @@ insert into  clothes (name, products_id) values ('Sweatpants', 8);
 insert into  clothes (name, products_id) values ('gloves', 10);
 insert into  clothes (name, products_id) values ('cap', 11);
 
---accessories
+--voorwerpen
 insert into  accessories (name, products_id) values ('straps', 3);
 insert into  accessories (name, products_id) values ('Powerlifting', 4);
 insert into  accessories (name, products_id) values ('protein', 7);
